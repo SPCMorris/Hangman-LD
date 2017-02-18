ef94d2946fb9e307878cad12f511b420a83e8130
Ryan Morris <ryanmorris.j@gmail.com>
2017-02-18 15:37:04 -0800

    [Fix] Working on Heroku deply

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -1,9 +1,7 @@
-const UserCtrl = require('./userController.js');
 const GameCtrl = require('./gameController.js');
 
 // Aggregates all controllers to export as one to reduce redundancy and help with debugging
 const Routes = {
-  UserCtrl,
   GameCtrl
 };
 
