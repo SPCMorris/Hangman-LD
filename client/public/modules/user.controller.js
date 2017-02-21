b648e35845a564b08f051fa286e749b0463e1e34
Ryan Morris <ryanmorris.j@gmail.com>
2017-02-20 20:05:21 -0800

    [Fix] Added log for debugging UserCtrl

--- client/public/modules/user.controller.js
+++ client/public/modules/user.controller.js
@@ -10,6 +10,7 @@ const UserCtrl = (function() {
   const userApiCall = (input) => {
     axios.get('user?' + 'nickname=' + input.nickname + '&secret=' + input.secret)
     .then( (resp) => {
+      console.log(resp)
       userObj["id"] = resp.data.id;
       userObj["nickname"] = resp.data.nickname;
       userObj["score"] = resp.data.score;
